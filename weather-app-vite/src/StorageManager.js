b80ca541784fd3cc34ee478d1214a9ea89ef4876
sseraflm <anomyuslol@gmail.com>
2026-08-29 00:50:40 +0200

    chore: scaffold Vite project structure

---
+++ weather-app-vite/src/StorageManager.js
@@ -0,0 +1,16 @@
+export class StorageManager {
+    constructor() {
+        this.historyKey = "History"
+        this.quickSearchKey = "quickSearch"
+    }
+
+    // fetchWeatherData
+    saveHistory(searches) {}
+
+    loadHistory() {}
+
+    // addToQuickSearch
+    saveQuickSearch(cities) {}
+
+    loadQuickSearch() {}
+}
---
+++ weather-app-vite/src/main.js
@@ -0,0 +1,3 @@
+import { WeatherService } from "./WeatherService.js";
+import { UIManager } from "./UIManager.js";
+import { StorageManager } from "./StorageManager.js";
