7d4a04c41490f95671e90ff4dbc38add07412636
sseraflm <anomyuslol@gmail.com>
2026-09-12 01:09:48 +0200

    feat: connect scss into the project

--- src/main.js
+++ src/main.js
@@ -1,94 +1,94 @@
 import { WeatherService } from "./WeatherService.js";
 import { UIManager } from "./UIManager.js";
 import { StorageManager } from "./StorageManager.js";
-
+import "./scss/main.scss";
 
 const weatherService = new WeatherService();
 const uiManager = new UIManager();
 const storageManager = new StorageManager();
 
 async function handleFetchWeather() {
-    const cityName = uiManager.getCityInputValue();
-    if (cityName === "") {
-        uiManager.renderError("The input is empty.")
-        return;
-    }
-    uiManager.setLoadingState(true)
-    try {
-        const { cityData, weatherData} = await weatherService.getWeatherForCity(cityName)
-        uiManager.renderWeatherCard(cityData, weatherData)
-        const historyObject = {
-            city: cityData.results[0].name,
-            time: weatherData.current.time,
-            temperature: weatherData.current.temperature_2m,
+  const cityName = uiManager.getCityInputValue();
+  if (cityName === "") {
+    uiManager.renderError("The input is empty.");
+    return;
+  }
+  uiManager.setLoadingState(true);
+  try {
+    const { cityData, weatherData } =
+      await weatherService.getWeatherForCity(cityName);
+    uiManager.renderWeatherCard(cityData, weatherData);
+    const historyObject = {
+      city: cityData.results[0].name,
+      time: weatherData.current.time,
+      temperature: weatherData.current.temperature_2m,
     };
-    const savedData = storageManager.loadHistory()
-    savedData.unshift(historyObject)
+    const savedData = storageManager.loadHistory();
+    savedData.unshift(historyObject);
     if (savedData.length > 5) {
-            savedData.pop();
-        }
-    
-    storageManager.saveHistory(savedData)
-    uiManager.renderHistory(savedData)
-    } catch(error) {
-        uiManager.renderError(error.message);
-    } finally {
-        uiManager.setLoadingState(false)
+      savedData.pop();
     }
+
+    storageManager.saveHistory(savedData);
+    uiManager.renderHistory(savedData);
+  } catch (error) {
+    uiManager.renderError(error.message);
+  } finally {
+    uiManager.setLoadingState(false);
+  }
 }
 
-uiManager.onSubmitClick(handleFetchWeather)
+uiManager.onSubmitClick(handleFetchWeather);
 
-const savedHistory = storageManager.loadHistory() 
-uiManager.renderHistory(savedHistory)
+const savedHistory = storageManager.loadHistory();
+uiManager.renderHistory(savedHistory);
 
 function handleAddToQuickSearch() {
-    const cityName = uiManager.getCityInputValue();
-    if (cityName === "") {
-        return;
-    }
-    const savedQuickSearch = storageManager.loadQuickSearch();
-    if (savedQuickSearch.some(city => city === cityName)) {
-        return;
-    }
-    savedQuickSearch.push(cityName)
-    storageManager.saveQuickSearch(savedQuickSearch);
-    uiManager.clearInput();
-    uiManager.renderQuickSearch(savedQuickSearch);
+  const cityName = uiManager.getCityInputValue();
+  if (cityName === "") {
+    return;
+  }
+  const savedQuickSearch = storageManager.loadQuickSearch();
+  if (savedQuickSearch.some((city) => city === cityName)) {
+    return;
+  }
+  savedQuickSearch.push(cityName);
+  storageManager.saveQuickSearch(savedQuickSearch);
+  uiManager.clearInput();
+  uiManager.renderQuickSearch(savedQuickSearch);
 }
 
-uiManager.onQuickSearchClick(handleAddToQuickSearch)
-
+uiManager.onQuickSearchClick(handleAddToQuickSearch);
 
 function quickSearchHandler(event) {
-    if (event.target.classList.contains("quick-search-city")) {
-        const cityP = event.target.innerText;
-        uiManager.setInputValue(cityP)
-        handleFetchWeather();
-    } else if (event.target.classList.contains("quick-search-delete")) {
-        const cityP = event.target.previousElementSibling.innerText   
-        const quickSearchData = storageManager.loadQuickSearch();     
-        if (quickSearchData.some(city => city === cityP)) {
-            const quickSearch = quickSearchData.filter(city => city !== cityP)
-            storageManager.saveQuickSearch(quickSearch)
-            uiManager.renderQuickSearch(quickSearch)
-        }
+  if (event.target.classList.contains("quick-search-city")) {
+    const cityP = event.target.innerText;
+    uiManager.setInputValue(cityP);
+    handleFetchWeather();
+  } else if (event.target.classList.contains("quick-search-delete")) {
+    const cityP = event.target.previousElementSibling.innerText;
+    const quickSearchData = storageManager.loadQuickSearch();
+    if (quickSearchData.some((city) => city === cityP)) {
+      const quickSearch = quickSearchData.filter((city) => city !== cityP);
+      storageManager.saveQuickSearch(quickSearch);
+      uiManager.renderQuickSearch(quickSearch);
     }
-
+  }
 }
 
-const savedQuickSearch = storageManager.loadQuickSearch()
-uiManager.renderQuickSearch(savedQuickSearch)
+const savedQuickSearch = storageManager.loadQuickSearch();
+uiManager.renderQuickSearch(savedQuickSearch);
 
-
-uiManager.onQuickSearchContainerClick(quickSearchHandler)
+uiManager.onQuickSearchContainerClick(quickSearchHandler);
 
 function handleHistoryClick(event) {
-    const historyItem = event.target.closest(".history-item")
-    if (!historyItem) {return;}
-    const cityName = historyItem.dataset.city;
-    uiManager.setInputValue(cityName)
-    handleFetchWeather()
+  const historyItem = event.target.closest(".history-item");
+  if (!historyItem) {
+    return;
+  }
+  const cityName = historyItem.dataset.city;
+  uiManager.setInputValue(cityName);
+  handleFetchWeather();
 }
 
-uiManager.onHistoryClick(handleHistoryClick)
+uiManager.onHistoryClick(handleHistoryClick);
